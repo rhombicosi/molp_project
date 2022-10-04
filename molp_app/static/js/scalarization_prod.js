0b9c8496faea3d78defa520545e622acd43bad2b
rhombicosi <ok.00100@gmail.com>
2022-10-04 12:36:20 +0200

    upload static files

---
+++ molp_app/static/js/scalarization_prod.js
@@ -0,0 +1,26 @@
+var socket = new WebSocket('ws://' + window.location.host + ':8001/ws/scalarizations/')
+
+socket.onmessage = function(event){
+        var tasks = event.data;
+        tasks = JSON.parse(tasks);
+        console.log(tasks);
+
+        for (let i = 0; i < tasks.length; i++) {
+        problem_id = tasks[i].problem_pk;
+        status = tasks[i].task_status;
+
+        var a = $('#process-raw-data-'+problem_id);
+        var dwnldbtn = $('#download-ch-'+problem_id);            
+
+        a.html(status);
+
+        if (status != 'SUCCESS') {
+            dwnldbtn.css('visibility', 'hidden');              
+            a.html(status);
+        }
+        else if (status == 'SUCCESS') {
+            dwnldbtn.css('visibility', 'visible');
+            a.html(status);
+        }
+    }
+}
---
+++ molp_app/static/js/scalarization_user_prod.js
@@ -0,0 +1,25 @@
+var user_socket = new WebSocket('ws://' + window.location.host + ':8001/ws/user_scalarizations/')
+        
+user_socket.onmessage = function(event){
+    var tasks = event.data;
+    tasks = JSON.parse(tasks)
+    
+    for (let i = 0; i < tasks.length; i++) {
+        problem_id = tasks[i].problem_pk
+        status = tasks[i].task_status
+
+        var a = $('#process-raw-data-user-'+problem_id);
+        var dwnldbtn = $('#download-ch-user-'+problem_id);
+
+        a.html(status);
+
+        if (status != 'SUCCESS') {
+            dwnldbtn.css('visibility', 'hidden');
+            a.html(status);
+        }
+        else if (status == 'SUCCESS') {
+            dwnldbtn.css('visibility', 'visible');
+            a.html(status);
+        }
+    }
+}
